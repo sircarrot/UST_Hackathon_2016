c8ff7f4e3a872b7adeda9643db081be81156b647
roycesanto <roycesanto@yahoo.com>
2016-04-17 00:27:11 +0800

    item lifespan 10 seconds

---
+++ Assets/_Royce/Scripts/Item.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Item : MonoBehaviour {
+
+    float timer;
+
+	// Use this for initialization
+	void Start () {
+        timer = Time.time;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if(Time.time - timer >= 10)
+        {
+            Destroy(gameObject);
+        }
+	}
+}
