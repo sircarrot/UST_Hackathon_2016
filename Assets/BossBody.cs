e169c252a93380437ab4749c90889fc25552b956
Brian So <briansostudio@gmail.com>
2016-04-17 10:24:47 +0800

    UI Fixes

---
+++ Assets/BossBody.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BossBody : MonoBehaviour {
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            PlayerController controller = other.GetComponent<PlayerController>();
+            controller.GetHit();
+        }
+    }
+}
